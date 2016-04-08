6f183ee7a3399179beed4d08138329b8cdca2529
Marius Glais <marius.glais@gmail.com>
2016-04-08 05:25:20 +0200

    Types : mise en place du CRUD

--- app/Type_materiels.php
+++ app/Type_materiels.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Type_materiels extends Model
-{
-    //
+class Type_materiels extends Model {
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function materiels() {
+        return $this->hasMany('App\Materiels');
+    }
+
 }
