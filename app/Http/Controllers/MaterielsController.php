609bc74b560967e446efe6f37327cae0914bc1ef
Marius Glais <marius.glais@gmail.com>
2016-04-08 05:25:33 +0200

    Matériels : mise en place du crud

---
+++ app/Http/Controllers/MaterielsController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Salles;
+use App\Type_materiels;
+use App\Http\Requests;
+use App\Materiels;
+
+class MaterielsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+
+        $salles = Salles::get();
+        $type_materiels = Type_materiels::get();
+
+        return view('materiels.create', compact('salles', 'type_materiels'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+
+        //dd($request->all());
+        $materiel = Materiels::create($request->all());
+        $type = Type_materiels::findOrFail($request['type_materiel_id']);
+        $materiel->type_materiel()->associate($type);
+        $materiel->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Materiels.php
+++ app/Materiels.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Materiels extends Model
-{
-    //
+class Materiels extends Model {
+
+    protected $fillable = [
+        'name', 'serial_number', 'bought', 'price'
+    ];
+
+    public function salles() {
+        return $this->belongsTo('App\Salles');
+    }
+
+    public function type_materiel() {
+        return $this->belongsTo('App\Type_materiels');
+    }
+
+    public function caracteristiques() {
+        return $this->hasMany('App\Caracteristiques');
+    }
 }
