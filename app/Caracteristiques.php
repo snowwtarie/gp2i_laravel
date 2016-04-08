0e91ba184b1ef4e5c6a54667e70bf360a4db53b3
Marius Glais <marius.glais@gmail.com>
2016-04-08 05:24:55 +0200

    Caracterisitques : mise en plus du CRUD

--- app/Caracteristiques.php
+++ app/Caracteristiques.php
@@ -4,7 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Caracteristiques extends Model
-{
-    //
+class Caracteristiques extends Model {
+
+    protected $fillable = [
+        'name', 'value', 'type_materiel_id'
+    ];
+
+    public function materiel() {
+        return $this->belongsTo('App\Materiels');
+    }
+
+    public function type_materiel() {
+        return $this->belongsTo('App\Type_materiels');
+    }
+
 }
